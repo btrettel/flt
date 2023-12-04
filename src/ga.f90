e0b9776cbbcc4f7790050855c3595fa32ac3e6a2
Ben Trettel <github@trettel.org>
2023-12-03 20:35:38 -0500

    refactor: Changing the names of some variables in ga.f90.

--- src/ga.f90
+++ src/ga.f90
@@ -9,39 +9,57 @@
 ! Project: [flt](https://github.com/btrettel/flt)
 ! License: [GPLv3](https://www.gnu.org/licenses/gpl-3.0.en.html)
 
+! Notation follows luke_essentials_2013 in most instances.
+
 module ga
 
 use prec, only: I5, RP
 implicit none
 private
 
-integer, public, parameter :: MAX_N_POP   = 64_I5 ! maximum population size
-integer, public, parameter :: MAX_N_GENES = 16_I5 ! maximum number of genes
-integer, public, parameter :: N_FITNESS   = 8_I5  ! number of fitness/objective function values
+! maximum population size
+integer, public, parameter :: MAX_N_POP = 64_I5
+
+! maximum number of components
+integer, public, parameter :: MAX_N_COMP = 16_I5
+
+! number of quality function values
+integer, public, parameter :: N_QUALITY = 8_I5
 
 type, public :: ga_config
-    real(kind=RP)    :: p_select, p_elite, p_cross, p_mutate
-    integer(kind=I5) :: n_gener, n_stall, stop_time
+    real(kind=RP)    :: p_select, p_elite, p_cross, p_mutate, stop_time
+    integer(kind=I5) :: n_gener, n_stall
 end type ga_config
 
 type, public :: bounds_type
-    real(kind=RP) :: lower, upper ! lower and upper bounds
+    ! lower and upper bounds
+    real(kind=RP) :: lower, upper
 end type bounds_type
 
-type, public :: chromo_type
-    real(kind=RP)    :: genes(MAX_N_GENES)
-    integer(kind=I5) :: n_genes
-    logical          :: set                ! whether the fitness/objective function has been set
-    real(kind=RP)    :: fitness(N_FITNESS) ! fitness/objective function values
-    !integer(kind=I5) :: id                 ! unique identification number
-end type chromo_type
+type, public :: candidate_type
+    real(kind=RP) :: components(MAX_N_COMP)
+    
+    ! number of components actually used
+    integer(kind=I5) :: n_comp
+    
+    ! whether the quality function(s) has/have been set
+    logical :: set
+    
+    ! quality function values
+    real(kind=RP) :: quality(N_QUALITY)
+end type candidate_type
 
 type, public :: pop_type
-    type(chromo_type) :: chromo(MAX_N_POP)
-    type(bounds_type) :: bounds(MAX_N_GENES)
-    integer(kind=I5)  :: n_pop, n_genes
-    type(chromo_type) :: best_pop_chromo, best_ever_chromo
-    !integer(kind=I5) :: max_id ! maximum identification number
+    type(candidate_type) :: candidates(MAX_N_POP)
+    type(bounds_type)    :: bounds(MAX_N_COMP)
+    
+    ! number of candidates in population
+    integer(kind=I5)  :: n_pop
+    
+    ! number of components actually used
+    integer(kind=I5)  :: n_comp
+    
+    type(candidate_type) :: best_pop_candidate, best_ever_candidate
 end type pop_type
 
 public :: rand_int, rand_cauchy
@@ -49,27 +67,32 @@ public :: clip
 
 contains
 
-function rand_int(lb, ub, r)
-    integer(kind=I5), intent(in) :: lb, ub
+function rand_int(lower_bound, upper_bound, r)
+    integer(kind=I5), intent(in) :: lower_bound, upper_bound
     real(kind=RP), intent(in)    :: r
     
     integer(kind=I5) :: rand_int
     
-    ! The `min` function makes this not return `ub + 1_I5` when `r = 1.0_RP`.
-    rand_int = min(lb + floor(real(ub + 1_I5 - lb, RP) * r), ub)
+    ! The `min` function makes this not return `upper_bound + 1_I5` when `r = 1.0_RP`.
+    rand_int = min(lower_bound + floor(real(upper_bound + 1_I5 - lower_bound, RP) * r), upper_bound)
     
     return
 end function rand_int
 
 function rand_cauchy(m, b, r)
+    ! Returns a Cauchy random variable with inverse transform sampling.
     ! Notation follows <https://mathworld.wolfram.com/CauchyDistribution.html>.
-    ! TODO: Describe what `m` and `b` represent in comments.
     
     use prec, only: PI
     
-    real(kind=RP), intent(in) :: m ! TODO median?
-    real(kind=RP), intent(in) :: b ! TODO scale factor similar to standard deviation?
-    real(kind=RP), intent(in) :: r ! random CDF value for inverse sampling
+    ! median
+    real(kind=RP), intent(in) :: m
+    
+    ! half width
+    real(kind=RP), intent(in) :: b
+    
+    ! random CDF value for inverse sampling
+    real(kind=RP), intent(in) :: r
     
     real(kind=RP) :: rand_cauchy
     
@@ -90,12 +113,17 @@ subroutine clip(bounds, x)
     return
 end subroutine clip
 
-!subroutine optimize(bounds, fun, best_ever_chromo, rc)
-!    type(bounds_type), intent(in)  :: bounds(:)
-!    type(chromo_type), intent(out) :: best_ever_chromo
-!    integer(kind=I5), intent(out)  :: rc
-!    
-!    ! TODO: `fun`
+!subroutine optimize(bounds, f, best_ever_candidate, rc)
+!    type(bounds_type), intent(in) :: bounds(:)
+!    type(candidate_type), intent(out) :: best_ever_candidate
+!    integer(kind=I5), intent(out) :: rc
+    
+!    interface
+!        function f(x)
+!            type(individual_type), intent(in) :: x
+!            real(kind=RP)                     :: f(:)
+!        end function fun
+!    end interface
 !end subroutine optimize
 
 end module ga
