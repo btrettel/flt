9e0efc708de7794445fb6dfdce0039a3e9de4b1b
Ben Trettel <github@trettel.org>
2024-05-05 23:06:35 -0400

    test: Tested read_config_namelist.

--- src/gen_units_mod.f90
+++ src/gen_units_mod.f90
@@ -9,7 +9,7 @@ module gen_units_mod
 
 use prec, only: WP
 use nmllog, only: log_type
-use units_types, only: MAX_LABEL_LEN, unit_type
+use units_types, only: MAX_LABEL_LEN, BASE_UNIT_LEN, unit_type
 implicit none
 private
 
@@ -26,6 +26,8 @@ type, public :: config_type
     
     type(log_type) :: logger
     
+    character(len=BASE_UNIT_LEN), allocatable :: base_units(:)
+    
     type(unit_type), allocatable              :: use_units(:)
     character(len=MAX_LABEL_LEN), allocatable :: use_labels(:)
 end type config_type
@@ -37,14 +39,14 @@ subroutine read_config_namelist(filename, config_out, rc)
     use units_types, only: MAX_BASE_UNITS, BASE_UNIT_LEN
     
     use prec, only: CL
-    !use checks, only: is_close
+    use checks, only: is_close
     use nmllog, only: INFO_LEVEL
     
     character(len=*), intent(in)   :: filename
     type(config_type), intent(out) :: config_out
     integer, intent(out)           :: rc
     
-    integer           :: i_base_unit, nml_unit, rc_nml, n_base_units
+    integer           :: i_base_unit, nml_unit, rc_nml, n_failures, n_base_units
     character(len=CL) :: nml_error_message
     
     ! `config` namelist group
@@ -53,7 +55,7 @@ subroutine read_config_namelist(filename, config_out, rc)
     real(kind=WP)                :: min_exponents(MAX_BASE_UNITS), max_exponents(MAX_BASE_UNITS)
     !logical                      :: tests, comparison, sqrt, cbrt, square, instrinsics
     
-    namelist /config/ output_file, base_units, type_definition, min_exponents, max_exponents
+    namelist /config/ output_file, base_units, type_definition, use_line, min_exponents, max_exponents
     
     call config_out%logger%open(GEN_UNITS_LOG, level=INFO_LEVEL)
     
@@ -96,6 +98,7 @@ subroutine read_config_namelist(filename, config_out, rc)
     config_out%use_line        = trim(use_line)
     config_out%min_exponents   = min_exponents(1:n_base_units)
     config_out%max_exponents   = max_exponents(1:n_base_units)
+    config_out%base_units      = base_units(1:n_base_units)
 !    config_out%tests           = tests
 !    config_out%comparison      = comparison
 !    config_out%sqrt            = sqrt
@@ -103,7 +106,24 @@ subroutine read_config_namelist(filename, config_out, rc)
 !    config_out%square          = square
 !    config_out%instrinsics     = instrinsics
     
-    rc = 0
+    n_failures = 0
+    
+    call config_out%logger%check(n_base_units > 0, "base_units must have 1 or more members", n_failures)
+    call config_out%logger%check(len(type_definition) > 0, "type_definition must have 1 or more characters", n_failures)
+    
+    do i_base_unit = 1, n_base_units
+        call config_out%logger%check(.not. is_close(min_exponents(i_base_unit), -huge(1.0_WP)), &
+                                        "min_exponents is not set properly?", n_failures)
+        call config_out%logger%check(.not. is_close(max_exponents(i_base_unit), huge(1.0_WP)), &
+                                        "max_exponents is not set properly?", n_failures)
+    end do
+    
+    if (n_failures > 0) then
+        call config_out%logger%error("config namelist input validation error(s)")
+        rc = n_failures
+    else
+        rc = 0
+    end if
 end subroutine read_config_namelist
 
 end module gen_units_mod
--- test/test_gen_units_mod.f90
+++ test/test_gen_units_mod.f90
@@ -8,7 +8,7 @@
 program test_gen_units_mod
 
 use gen_units_mod, only: config_type, read_config_namelist
-!use prec, only: WP
+use prec, only: WP
 use nmllog, only: log_type
 use unittest, only: test_results_type
 implicit none
@@ -23,7 +23,24 @@ call tests%start_tests(logger)
 
 call read_config_namelist("test/units.nml", config, rc)
 
-call tests%integer_eq(rc, 0, "read_config_namelist rc")
+! TODO: `type_definition` and `use_line` are set to the defaults below.
+! Later use a different input file to check every member of `config`.
+
+! TODO: Test what the defaults are to make mutation testing happy.
+
+call tests%character_eq(config%output_file, "src/pdim_types.f90", "read_config_namelist, config%output_file")
+call tests%character_eq(config%type_definition, "real(kind=WP)", "read_config_namelist, config%type_definition")
+call tests%character_eq(config%use_line, "use prec, only: WP", "read_config_namelist, config%use_line")
+call tests%real_eq(config%min_exponents(1), 0.0_WP, "read_config_namelist, config%min_exponents(1)")
+call tests%real_eq(config%min_exponents(2), 0.0_WP, "read_config_namelist, config%min_exponents(2)")
+call tests%real_eq(config%min_exponents(3), -1.0_WP, "read_config_namelist, config%min_exponents(3)")
+call tests%real_eq(config%max_exponents(1), 3.0_WP, "read_config_namelist, config%max_exponents(1)")
+call tests%real_eq(config%max_exponents(2), 0.0_WP, "read_config_namelist, config%max_exponents(2)")
+call tests%real_eq(config%max_exponents(3), 1.0_WP, "read_config_namelist, config%max_exponents(3)")
+call tests%character_eq(config%base_units(1), "kg", "read_config_namelist, config%base_units(1)")
+call tests%character_eq(config%base_units(2), "m", "read_config_namelist, config%base_units(2)")
+call tests%character_eq(config%base_units(3), "s", "read_config_namelist, config%base_units(3)")
+call tests%integer_eq(rc, 0, "read_config_namelist, rc")
 
 call tests%end_tests()
 call logger%close()
