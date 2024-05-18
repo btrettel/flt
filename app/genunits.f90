cc6bb22c875a9361eae31bb72d520f096aef2a27
Ben Trettel <github@trettel.org>
2024-05-18 19:52:08 -0400

    build: Completed draft version of depends.py.

--- app/genunits.f90
+++ app/genunits.f90
@@ -5,7 +5,7 @@
 ! Project: [flt](https://github.com/btrettel/flt)
 ! License: [GPLv3](https://www.gnu.org/licenses/gpl-3.0.en.html)
 
-program gen_units
+program genunits
 
 use genunits_data, only: unit_system_type
 use genunits_io, only: config_type
@@ -44,4 +44,4 @@ call config%logger%close()
 deallocate(input_file)
 deallocate(unit_system%units)
 
-end program gen_units
+end program genunits
