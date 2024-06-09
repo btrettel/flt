1ec3840e854f56e155d5cc29ac48d45f3a21589a
Ben Trettel <github@trettel.org>
2024-06-09 14:21:46 -0400

    feat: Added script to create revision.f90 for SVN.

---
+++ py/svnrev.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env -S python3 -Werror
+# -*- coding: utf-8 -*-
+
+import os
+from subprocess import run
+
+with open(os.path.join("src", "revision.f90"), "w") as output_handler:
+    output_handler.write("character(len=*), public, parameter :: REVISION = \"")
+    result = run(["svn", "info", "--show-item", "revision"], capture_output=True, text=True)
+    output_handler.write(result.stdout.strip())
+    output_handler.write("\"\n")
+    
+    output_handler.write("character(len=*), public, parameter :: REVISION_DATE = \"")
+    result = run(["svn", "info", "--show-item", "last-changed-date"], capture_output=True, text=True)
+    output_handler.write(result.stdout.strip())
+    output_handler.write("\"\n")
+    
+    output_handler.write("logical, public, parameter :: MODIFIED = ")
+    result = run(["svn", "diff", "--summarize"], capture_output=True, text=True)
+    if len(result.stdout.strip()) > 0:
+        output_handler.write(".true.\n")
+    else:
+        output_handler.write(".false.\n")
