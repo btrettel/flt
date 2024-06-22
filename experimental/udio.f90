7f3fbd10557b5d50e80f1da7fb24986c9e3ba834
Ben Trettel <github@trettel.org>
2024-06-22 14:39:47 -0400

    feat: Trying derived-type input with namelists. Works better than I expected, though compiler support for some details is lacking.

--- experimental/udio.f90
+++ experimental/udio.f90
@@ -6,7 +6,9 @@ type meter
     real :: v
 contains
     procedure :: meter_wf
+    procedure :: meter_rf
     generic   :: write(formatted) => meter_wf
+    generic   :: read(formatted) => meter_rf
 end type meter
 
 contains
@@ -26,8 +28,60 @@ subroutine meter_wf(dtv, unit, iotype, vlist, iostat, iomsg)
     else
         write(pfmt, "(2a, i0, a, i0, a)") "(", iotype(3:), vlist(1), ".", vlist(2), ", a)"
     end if
-    write(unit, fmt=trim(pfmt), iostat=iostat) dtv%v, " m"
+    write(unit, fmt=trim(pfmt), iostat=iostat, iomsg=iomsg) dtv%v, " m"
 end subroutine meter_wf
+
+subroutine meter_rf(dtv, unit, iotype, vlist, iostat, iomsg)
+    class(meter), intent(in out)     :: dtv
+    integer, intent(in)              :: unit
+    character(len=*), intent(in)     :: iotype
+    integer, intent(in)              :: vlist(:)
+    integer, intent(out)             :: iostat
+    character(len=*), intent(in out) :: iomsg
+    
+    character(len=128) :: full_input, value_char, unit_char
+    integer            :: space_index
+    
+    read(unit, fmt="(a)", iostat=iostat) full_input
+    
+    ! Needed for ifx namelist input.
+    full_input = adjustl(full_input)
+    
+    space_index = index(full_input, " ")
+    
+    value_char = full_input(1:space_index-1)
+    read(unit=value_char, fmt=*, iostat=iostat) dtv%v
+    
+    unit_char = full_input(space_index+1:)
+    
+    print *, trim(unit_char)
+    
+    if (len(trim(unit_char)) == 0) then
+        iostat = -1
+        iomsg = "Unit expected, none appeared: " // trim(full_input)
+        return
+    end if
+    
+    if (trim(unit_char) /= "m") then
+        iostat = -2
+        iomsg = "Unit mismatch: m expected, " // trim(unit_char) // " appeared."
+        return
+    end if
+    
+    if (iotype /= "DT") then
+        iostat = -3
+        iomsg = 'Only iotype="DT" is implemented for read. iotype=' // iotype
+        return
+    end if
+    
+    if (size(vlist) > 0) then
+        iostat = -4
+        iomsg = "vlist must be not specified for read."
+        return
+    end if
+    
+    iomsg = ""
+end subroutine meter_rf
 
 end module udio_mod
 
@@ -36,11 +90,15 @@ program udio
 use udio_mod
 implicit none
 
-type(meter) :: x
+type(meter)        :: x
+character(len=24)  :: mchar
+integer            :: nml_unit
+integer            :: rc
+character(len=128) :: rmsg
 
-x%v = 10.123
+namelist /list/ x
 
-! TODO: try `iotype`
+x%v = 54.321
 
 ! TODO: Check latest gfortran to see if it can handle a zero width case.
 ! 0 trident$ gfortran udio.f90 
@@ -54,5 +112,30 @@ write(*, fmt="(dt'f'(6,3))") x
 write(*, fmt="(dt'en'(10,3))") x
 write(*, fmt="(dt'g0')") x ! Workaround for gfortran not accepting widths of 0.
 write(*, fmt=*) x
+
+! nvfortran can't do these. nvfortran does seem to have problems `read`ing from internal variables.
+mchar = "12.345 m"
+read(mchar, fmt="(dt)", iostat=rc, iomsg=rmsg) x
+write(*, fmt="(dt'f'(6,3))") x
+write(*, *) rc, rmsg
+
+mchar = "21.543"
+read(mchar, fmt="(dt)", iostat=rc, iomsg=rmsg) x
+write(*, fmt="(dt'f'(6,3))") x
+write(*, *) rc, rmsg
+
+mchar = "32.154 s"
+read(mchar, fmt="(dt)", iostat=rc, iomsg=rmsg) x
+write(*, fmt="(dt'f'(6,3))") x
+write(*, *) rc, rmsg
+
+! Namelist `read`s work on nvfortran. But `iostat` and `iomsg` seem to be broken.
+open(newunit=nml_unit, file="x.nml", status="old", action="read", iostat=rc, iomsg=rmsg)
+!open(newunit=nml_unit, file="x.nml", status="old", action="read")
+read(unit=nml_unit, nml=list)
+close(nml_unit)
+
+write(*, fmt="(dt'f'(6,3))") x
+write(*, *) rc, rmsg
 
 end program udio
