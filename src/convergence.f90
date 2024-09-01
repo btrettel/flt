42f89f4fc124826be5de8f53c8318bb62920b166
Ben Trettel <12763077+btrettel@users.noreply.github.com>
2024-09-01 18:47:09 -0400

    refactor: Made some error messages more consistent.

--- src/convergence.f90
+++ src/convergence.f90
@@ -183,15 +183,15 @@ subroutine convergence_test(n_arr, solver_de, p_expected, message, tests, p_tol)
                                                 // "which probably isn't desired")
         
         write(unit=i_var_string, fmt="(i0)") i_var
-        call tests%real_eq(p_v(i_var)%v, p_expected(i_var), message // ", p_v = expected, var=" // trim(i_var_string), &
+        call tests%real_eq(p_v(i_var)%v, p_expected(i_var), message // ", p_v=expected, var=" // trim(i_var_string), &
                                 abs_tol=p_tol_(i_var))
         
         do i_dv = 1, n_dv ! SERIAL
             write(unit=i_dv_string, fmt="(i0)") i_dv
             call tests%real_eq(p_v(i_var)%dv(i_dv), 0.0_WP, message // ", p_v%dv(" // trim(i_dv_string) &
-                                    // ") = 0, var=" // trim(i_var_string), abs_tol=p_tol_(i_var))
+                                    // ")=0, var=" // trim(i_var_string), abs_tol=p_tol_(i_var))
             call tests%real_eq(p_dv(i_var, i_dv), p_expected(i_var), message // ", p_dv(" // trim(i_dv_string) &
-                                    // ") = expected, var=" // trim(i_var_string), abs_tol=p_tol_(i_var))
+                                    // " =expected, var=" // trim(i_var_string), abs_tol=p_tol_(i_var))
         end do
     end do
 end subroutine convergence_test
