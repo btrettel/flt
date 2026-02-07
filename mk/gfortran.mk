bf0b7873093791cfbf314a198fc218991b55b8bf
Ben Trettel <12763077+btrettel@users.noreply.github.com>
2026-02-07 09:37:17 -0500

    feat: Added ability to print more in the version message in cli.f90.

--- mk/gfortran.mk
+++ mk/gfortran.mk
@@ -1,7 +1,8 @@
 # `-Werror=uninitialized` seems to require `-fcheck=all` (not used in the release build), or else there's a false-positive compile-time error, at least for gfortran 13. Check if this is true for later versions. `-Wno-uninitialized` eliminates this false-positive and should be removed in the future if gfortran eliminates this problem.
+# `-fsanitize=leak` seems to continue to have false positives as of gfortran 13.
 
 FFLAGS   = -Wall -Wextra -Werror -pedantic-errors -Wno-maybe-uninitialized -Wno-do-subscript -std=f2018 -Wconversion -Wconversion-extra -fimplicit-none -fno-unsafe-math-optimizations -finit-real=snan -finit-integer=-2147483647 -finit-logical=true -finit-derived -Wimplicit-interface -Wunused -ffree-line-length-132
-DFLAGS   = -Og -g -fbacktrace -fcheck=all -ffpe-trap=invalid,zero,overflow,underflow,denormal -fsanitize=leak --coverage
+DFLAGS   = -Og -g -fbacktrace -fcheck=all -ffpe-trap=invalid,zero,overflow,underflow,denormal --coverage
 RFLAGS   = -O2 -Wno-uninitialized -fopt-info-missed=$(MISSED) -flto
 AFLAGS   = 
 NFLAGS   = -march=native
