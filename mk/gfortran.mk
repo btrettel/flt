fcf46b0ed388db3dacdee5a8500b95fc23868396
Ben Trettel <12763077+btrettel@users.noreply.github.com>
2026-02-12 09:19:50 -0500

    build: Changed gfortran configuration in a way that would have caught an undefined variable previously missed.

--- mk/gfortran.mk
+++ mk/gfortran.mk
@@ -1,13 +1,20 @@
 # `-Werror=uninitialized` seems to require `-fcheck=all` (not used in the release build), or else there's a false-positive compile-time error, at least for gfortran 13. Check if this is true for later versions. `-Wno-uninitialized` eliminates this false-positive and should be removed in the future if gfortran eliminates this problem.
 # `-fsanitize=leak` seems to continue to have false positives as of gfortran 13.
+# `-Wdo-subscript` seems to have false positives.
+# I think I had some false positives before that made me add `-Wno-maybe-uninitialized`, but it seems unnecessary now.
 
-FFLAGS   = -Wall -Wextra -Werror -pedantic-errors -Wno-maybe-uninitialized -Wno-do-subscript -std=f2018 -Wconversion -Wconversion-extra -fimplicit-none -fno-unsafe-math-optimizations -finit-real=snan -finit-integer=-2147483647 -finit-logical=true -finit-derived -Wimplicit-interface -Wunused -ffree-line-length-132
+FFLAGS   = -Wall -Wextra -Werror -pedantic-errors -Wno-do-subscript -std=f2018 -Wconversion -Wconversion-extra -fimplicit-none -fno-unsafe-math-optimizations -finit-real=snan -finit-integer=2147483647 -finit-logical=true -finit-derived -Wimplicit-interface -Wunused -ffree-line-length-132
 DFLAGS   = -Og -g -fbacktrace -fcheck=all -ffpe-trap=invalid,zero,overflow,underflow,denormal --coverage
 RFLAGS   = -O2 -Wno-uninitialized -fopt-info-missed=$(MISSED) -flto
 AFLAGS   = 
 NFLAGS   = -march=native
 SFLAGS   = -static
 OMPFLAGS = -fopenmp
+
+# Making `-finit-integer=2147483647` is better than making it a large negative number.
+# Say you have a loop: `do i = 1, n` and you forget to initialize `n`.
+# If `n` is arbitrarily large then this will eventually return an out-of-bounds array error.
+# gfortran's uninitialized variable detection seems to not detect this.
 
 # <https://gcc.gnu.org/onlinedocs/gcc/Optimize-Options.html>
 
