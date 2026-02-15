29324958b682c5ee6a9d86e923ba2220d05ef546
Ben Trettel <12763077+btrettel@users.noreply.github.com>
2026-02-15 11:55:03 -0500

    feat: Got a rudimentary tripwire.py script working.

--- py/tripwire.py
+++ py/tripwire.py
@@ -7,6 +7,7 @@ import unittest
 
 TRIPWIRE_DIRECTIVE_PREFIX = "tripwire$"
 TRIPWIRE_BEGIN_DIRECTIVE  = TRIPWIRE_DIRECTIVE_PREFIX+" begin"
+TRIPWIRE_END_DIRECTIVE    = TRIPWIRE_DIRECTIVE_PREFIX+" end"
 
 def crc32(x):
     from zlib import crc32
@@ -19,14 +20,77 @@ def crc32(x):
     
     return checksum.upper()
 
-def get_tripwire(file, checksum)
+def get_fenced_lines(file, checksum):
+    fenced_lines = ""
+    
+    with open(file, "r") as file_in:
+        next_line_in_fence = False
+        line_no = 0
+        for line in file_in:
+            line_no += 1
+            
+            if next_line_in_fence:
+                if TRIPWIRE_END_DIRECTIVE in line:
+                    break
+                else:
+                    fenced_lines = fenced_lines + line
+            
+            if TRIPWIRE_BEGIN_DIRECTIVE in line:
+                line_checksum, message = parse_begin_directive(get_tripwrite_directive(line))
+                begin_line_no = line_no
+                if checksum == line_checksum:
+                    next_line_in_fence = True
+    
+    return fenced_lines, message, begin_line_no
+
+def get_reported_checksums(file):
+    reported_checksums = set()
+    
     with open(file, "r") as file_in:
         for line in file_in:
             if TRIPWIRE_BEGIN_DIRECTIVE in line:
-                
+                reported_checksum, _ = parse_begin_directive(get_tripwrite_directive(line))
+                reported_checksums.add(reported_checksum)
+    
+    return reported_checksums
+
+def get_tripwrite_directive(line):
+    assert TRIPWIRE_DIRECTIVE_PREFIX in line
+    
+    return line.strip()[line.strip().index(TRIPWIRE_DIRECTIVE_PREFIX):]
+
+def parse_begin_directive(directive):
+    assert directive.startswith(TRIPWIRE_BEGIN_DIRECTIVE)
+    
+    directive_split = directive.strip().split(" ")
+    assert directive_split[1] == "begin"
+    
+    checksum = directive_split[2]
+    message  = directive_split[3]
+    
+    return checksum, message
 
 if __name__ == "__main__":
-    print("blah")
+    import argparse
+    import sys
+    
+    parser = argparse.ArgumentParser(description="Detects when specified lines of code change.")
+    parser.add_argument("files", nargs="*", help="input file(s) to read")
+    args = parser.parse_args()
+    
+    exit_code = 0
+    for file in args.files:
+        reported_checksums = get_reported_checksums(file)
+        
+        for reported_checksum in reported_checksums:
+            fenced_lines, message, line_no = get_fenced_lines(file, reported_checksum)
+            actual_checksum = crc32(fenced_lines)
+            
+            if actual_checksum != reported_checksum:
+                print("{}:{} (reported {}, actual {}): {}".format(file, line_no, reported_checksum, actual_checksum, message))
+                exit_code = 1
+    
+    sys.exit(exit_code)
 
 class Tests(unittest.TestCase):
     # <https://docs.python.org/3/library/unittest.html>
@@ -39,4 +103,13 @@ class Tests(unittest.TestCase):
         
         # <https://lxp32.github.io/docs/a-simple-example-crc32-calculation/>
         self.assertEqual(crc32("123456789"), "CBF43926")
+    
+    def test_get_tripwrite_directive(self):
+        self.assertEqual(get_tripwrite_directive("don't include this tripwire$ begin CHECKSUM message"), "tripwire$ begin CHECKSUM message")
+        self.assertEqual(get_tripwrite_directive("don't include this tripwire$ end"), "tripwire$ end")
+    
+    def test_parse_begin_directive(self):
+        checksum, message = parse_begin_directive("tripwire$ begin CHECKSUM message")
         
+        self.assertEqual(checksum, "CHECKSUM")
+        self.assertEqual(message, "message")
