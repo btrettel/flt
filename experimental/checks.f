3c226beb622f7aa0143adfb55be572338713e2ff
Ben Trettel <12763077+btrettel@users.noreply.github.com>
2026-04-15 23:41:01 -0400

    feat: Added parameter to F77 assert to subroutine to disable for releases.

--- experimental/checks.f
+++ experimental/checks.f
@@ -3,16 +3,20 @@
       CALL ASSERT(.TRUE., 'PASSING')
       CALL ASSERT(.FALSE., 'FAILING')
       END
-      
+
       SUBROUTINE ASSERT(CONDIT, MESSAG)
 C     Condition being asserted.
       LOGICAL CONDIT
 C     Message to output if CONDIT is .FALSE.
       CHARACTER*(*) MESSAG
-      
-      IF (.NOT. CONDIT) THEN
-          WRITE(*, *) 'ASSERTION FAILED.'
-          WRITE(*, *) MESSAG
-          STOP 1
+C     Change to .FALSE. to disable assertions.
+      LOGICAL DEBUG
+      PARAMETER (DEBUG = .TRUE.)
+      IF (DEBUG) THEN
+          IF (.NOT. CONDIT) THEN
+              WRITE(*, *) 'ASSERTION FAILED.'
+              WRITE(*, *) MESSAG
+              STOP 1
+          END IF
       END IF
       END
