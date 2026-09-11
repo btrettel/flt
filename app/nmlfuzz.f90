btrettel/flt

commit e38a44033ef53989e2299d09bb959a2925d69dcb

feat: Made nmlfuzz check for long run times.

app/nmlfuzz.f90
@@ -15,6 +15,7 @@ use geninput_io, only: config_type, input_variable_type, read_config_namelist, r
 use purerng, only: rng_type
 use checks, only: is_close
 use stopcodes, only: EX_OK
+use timer, only: timer_type
 implicit none
 
 character(len=*), parameter :: STOP_NOW_FILE = "stop_now"
@@ -28,8 +29,9 @@ integer(I10)      :: n_fuzz, n_failure
 type(rng_type)    :: rng
 character(4)      :: type4
 real(WP)          :: x, x_real
-logical           :: exit_code_is_acceptable, stop_now_detected
-type(input_variable_type), allocatable          :: input_variables(:)
+logical           :: result_is_acceptable, stop_now_detected
+type(timer_type)  :: wtime
+type(input_variable_type), allocatable :: input_variables(:)
 
 ! Read all namelists and exit if any have issues.
 call get_input_file_name_from_cli("nmlfuzz", input_file)
@@ -104,18 +106,23 @@ fuzzer_loop: do
     write(unit=out_unit, fmt="(a)") "/"
     close(unit=out_unit)
     
+    call wtime%start()
     call execute_command_line(config%executable // " " // nml_file, exitstat=exit_code)
+    call wtime%stop()
     
-    ! Check exit code to see if it's acceptable.
-    exit_code_is_acceptable = .false.
-    exit_code_loop: do i_exit_code = 1, size(config%acceptable_exit_codes)
-        if (exit_code == config%acceptable_exit_codes(i_exit_code)) then
-            exit_code_is_acceptable = .true.
-            exit exit_code_loop
-        end if
-    end do exit_code_loop
+    result_is_acceptable = wtime%read() < config%run_time_threshold
     
-    if (exit_code_is_acceptable) then
+    if (result_is_acceptable) then
+        ! Check exit code to see if it's acceptable.
+        exit_code_loop: do i_exit_code = 1, size(config%acceptable_exit_codes)
+            if (exit_code == config%acceptable_exit_codes(i_exit_code)) then
+                result_is_acceptable = .true.
+                exit exit_code_loop
+            end if
+        end do exit_code_loop
+    end if
+    
+    if (result_is_acceptable) then
         open(newunit=out_unit, status="old", file=trim(nml_file))
         close(unit=out_unit, status="delete")
     else

config.ini
@@ -1,6 +1,6 @@
 [f90lint]
 directories              = app src test
-skip_indexing            = src/debug.f90 src/release.f90 src/rev.f90 src/units.f90 src/units_ad.f90
+skip_indexing            = src/debug.f90 src/fuzz.f90 src/release.f90 src/rev.f90 src/units.f90 src/units_ad.f90
 ignore_assertion_density = 
 ignore_test_sloc_ratio   = src/cli.f90 src/genunits_io.f90 src/timer.f90
 ignore_branch_density    = 
@@ -14,7 +14,7 @@ directories = app src test
 no_existence_check = build rev
 
 # The `build` module (debug.f90 or release.f90, depending on the value of the `BUILD` make macro) disables assertions. Because this violates this program's assumption that the filename matches that of the module name (`build` is not the same as `debug` or `release`), I had to add some logic to this program to handle this case. Part of that includes ignoring the following files and manually adding a file with the filename `$(BUILD)` later.
-module_name_mismatch = src/debug.f90 src/release.f90
+module_name_mismatch = src/debug.f90 src/fuzz.f90 src/release.f90
 
 # The following are not always present, so they should not be indexed.
 skip_indexing = src/rev.f90

mk/before.mk
@@ -11,4 +11,4 @@ TESTNML = checks.nml fmad.nml ga.nml genunits_data.nml genunits_io.nml ooa.nml p
 # All source files #
 ####################
 
-ALLSRC = app$(DIR_SEP)geninput.f90 app$(DIR_SEP)gentesthtml.f90 app$(DIR_SEP)genunits.f90 app$(DIR_SEP)nmlfuzz.f90 mk$(DIR_SEP)before.mk mk$(DIR_SEP)build.md mk$(DIR_SEP)common.mk mk$(DIR_SEP)crayftn.mk mk$(DIR_SEP)depends.mk mk$(DIR_SEP)flang.mk mk$(DIR_SEP)gfortran.mk mk$(DIR_SEP)ifort_linux.mk mk$(DIR_SEP)ifort_windows.mk mk$(DIR_SEP)ifx_linux.mk mk$(DIR_SEP)ifx_windows.mk mk$(DIR_SEP)lfortran.mk mk$(DIR_SEP)linux_2.mk mk$(DIR_SEP)linux_defaults.mk mk$(DIR_SEP)manual.mk mk$(DIR_SEP)nagfor.mk mk$(DIR_SEP)nvfortran.mk mk$(DIR_SEP)windows_2.mk mk$(DIR_SEP)windows_defaults.mk mk$(DIR_SEP)xlf.mk src$(DIR_SEP)checks.f90 src$(DIR_SEP)cli.f90 src$(DIR_SEP)debug.f90 src$(DIR_SEP)fmad.f90 src$(DIR_SEP)ga.f90 src$(DIR_SEP)geninput_io.f90 src$(DIR_SEP)genunits_data.f90 src$(DIR_SEP)genunits_io.f90 src$(DIR_SEP)ooa.f90 src$(DIR_SEP)port.f90 src$(DIR_SEP)prec.f90 src$(DIR_SEP)purerng.f90 src$(DIR_SEP)release.f90 src$(DIR_SEP)stopcodes.f90 src$(DIR_SEP)timer.f90 src$(DIR_SEP)unittest.f90 src$(DIR_SEP)validation.f90 test$(DIR_SEP)genunits_input.nml test$(DIR_SEP)genunits_input_ad.nml test$(DIR_SEP)test_assert_dimension_false_1.f90 test$(DIR_SEP)test_assert_dimension_false_2.f90 test$(DIR_SEP)test_assert_dimension_false_3.f90 test$(DIR_SEP)test_assert_false_1.f90 test$(DIR_SEP)test_checks.f90 test$(DIR_SEP)test_fmad.f90 test$(DIR_SEP)test_ga.f90 test$(DIR_SEP)test_genunits_data.f90 test$(DIR_SEP)test_genunits_io.f90 test$(DIR_SEP)test_ooa.f90 test$(DIR_SEP)test_port.f90 test$(DIR_SEP)test_prec.f90 test$(DIR_SEP)test_purerng.f90 test$(DIR_SEP)test_rev.f90 test$(DIR_SEP)test_timer.f90 test$(DIR_SEP)test_units.f90 test$(DIR_SEP)test_units_ad.f90 test$(DIR_SEP)test_units_fail_1.f90 test$(DIR_SEP)test_units_fail_2.f90 test$(DIR_SEP)test_units_fail_3.f90 test$(DIR_SEP)test_units_fail_4.f90 test$(DIR_SEP)test_unittest.f90 test$(DIR_SEP)test_validation.f90
+ALLSRC = app$(DIR_SEP)geninput.f90 app$(DIR_SEP)gentesthtml.f90 app$(DIR_SEP)genunits.f90 app$(DIR_SEP)nmlfuzz.f90 mk$(DIR_SEP)before.mk mk$(DIR_SEP)build.md mk$(DIR_SEP)common.mk mk$(DIR_SEP)crayftn.mk mk$(DIR_SEP)depends.mk mk$(DIR_SEP)flang.mk mk$(DIR_SEP)gfortran.mk mk$(DIR_SEP)ifort_linux.mk mk$(DIR_SEP)ifort_windows.mk mk$(DIR_SEP)ifx_linux.mk mk$(DIR_SEP)ifx_windows.mk mk$(DIR_SEP)lfortran.mk mk$(DIR_SEP)linux_2.mk mk$(DIR_SEP)linux_defaults.mk mk$(DIR_SEP)manual.mk mk$(DIR_SEP)nagfor.mk mk$(DIR_SEP)nvfortran.mk mk$(DIR_SEP)windows_2.mk mk$(DIR_SEP)windows_defaults.mk mk$(DIR_SEP)xlf.mk src$(DIR_SEP)checks.f90 src$(DIR_SEP)cli.f90 src$(DIR_SEP)debug.f90 src$(DIR_SEP)fmad.f90 src$(DIR_SEP)fuzz.f90 src$(DIR_SEP)ga.f90 src$(DIR_SEP)geninput_io.f90 src$(DIR_SEP)genunits_data.f90 src$(DIR_SEP)genunits_io.f90 src$(DIR_SEP)ooa.f90 src$(DIR_SEP)port.f90 src$(DIR_SEP)prec.f90 src$(DIR_SEP)purerng.f90 src$(DIR_SEP)release.f90 src$(DIR_SEP)stopcodes.f90 src$(DIR_SEP)timer.f90 src$(DIR_SEP)unittest.f90 src$(DIR_SEP)validation.f90 test$(DIR_SEP)genunits_input.nml test$(DIR_SEP)genunits_input_ad.nml test$(DIR_SEP)test_assert_dimension_false_1.f90 test$(DIR_SEP)test_assert_dimension_false_2.f90 test$(DIR_SEP)test_assert_dimension_false_3.f90 test$(DIR_SEP)test_assert_false_1.f90 test$(DIR_SEP)test_checks.f90 test$(DIR_SEP)test_fmad.f90 test$(DIR_SEP)test_ga.f90 test$(DIR_SEP)test_genunits_data.f90 test$(DIR_SEP)test_genunits_io.f90 test$(DIR_SEP)test_ooa.f90 test$(DIR_SEP)test_port.f90 test$(DIR_SEP)test_prec.f90 test$(DIR_SEP)test_purerng.f90 test$(DIR_SEP)test_rev.f90 test$(DIR_SEP)test_timer.f90 test$(DIR_SEP)test_units.f90 test$(DIR_SEP)test_units_ad.f90 test$(DIR_SEP)test_units_fail_1.f90 test$(DIR_SEP)test_units_fail_2.f90 test$(DIR_SEP)test_units_fail_3.f90 test$(DIR_SEP)test_units_fail_4.f90 test$(DIR_SEP)test_unittest.f90 test$(DIR_SEP)test_validation.f90

mk/depends.mk
@@ -12,6 +12,8 @@ src$(DIR_SEP)debug.$(OBJEXT): src$(DIR_SEP)debug.f90
 
 src$(DIR_SEP)fmad.$(OBJEXT): src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)fmad.f90
 
+src$(DIR_SEP)fuzz.$(OBJEXT): src$(DIR_SEP)fuzz.f90
+
 src$(DIR_SEP)ga.$(OBJEXT): src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)purerng.$(OBJEXT) src$(DIR_SEP)ga.f90
 
 src$(DIR_SEP)geninput_io.$(OBJEXT): src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)port.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)geninput_io.f90
@@ -55,8 +57,8 @@ gentesthtml$(BINEXT): src$(DIR_SEP)$(BUILD).$(OBJEXT) src$(DIR_SEP)checks.$(OBJE
 genunits$(BINEXT): src$(DIR_SEP)$(BUILD).$(OBJEXT) src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)cli.$(OBJEXT) src$(DIR_SEP)genunits_data.$(OBJEXT) src$(DIR_SEP)genunits_io.$(OBJEXT) src$(DIR_SEP)port.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)rev.$(OBJEXT) src$(DIR_SEP)stopcodes.$(OBJEXT) app$(DIR_SEP)genunits.f90
 	$(FC) $(OFLAG) $@ $(FFLAGS) src$(DIR_SEP)$(BUILD).$(OBJEXT) src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)cli.$(OBJEXT) src$(DIR_SEP)genunits_data.$(OBJEXT) src$(DIR_SEP)genunits_io.$(OBJEXT) src$(DIR_SEP)port.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)rev.$(OBJEXT) src$(DIR_SEP)stopcodes.$(OBJEXT) app$(DIR_SEP)genunits.f90
 
-nmlfuzz$(BINEXT): src$(DIR_SEP)$(BUILD).$(OBJEXT) src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)cli.$(OBJEXT) src$(DIR_SEP)geninput_io.$(OBJEXT) src$(DIR_SEP)port.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)purerng.$(OBJEXT) src$(DIR_SEP)rev.$(OBJEXT) src$(DIR_SEP)stopcodes.$(OBJEXT) app$(DIR_SEP)nmlfuzz.f90
-	$(FC) $(OFLAG) $@ $(FFLAGS) src$(DIR_SEP)$(BUILD).$(OBJEXT) src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)cli.$(OBJEXT) src$(DIR_SEP)geninput_io.$(OBJEXT) src$(DIR_SEP)port.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)purerng.$(OBJEXT) src$(DIR_SEP)rev.$(OBJEXT) src$(DIR_SEP)stopcodes.$(OBJEXT) app$(DIR_SEP)nmlfuzz.f90
+nmlfuzz$(BINEXT): src$(DIR_SEP)$(BUILD).$(OBJEXT) src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)cli.$(OBJEXT) src$(DIR_SEP)geninput_io.$(OBJEXT) src$(DIR_SEP)port.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)purerng.$(OBJEXT) src$(DIR_SEP)rev.$(OBJEXT) src$(DIR_SEP)stopcodes.$(OBJEXT) src$(DIR_SEP)timer.$(OBJEXT) app$(DIR_SEP)nmlfuzz.f90
+	$(FC) $(OFLAG) $@ $(FFLAGS) src$(DIR_SEP)$(BUILD).$(OBJEXT) src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)cli.$(OBJEXT) src$(DIR_SEP)geninput_io.$(OBJEXT) src$(DIR_SEP)port.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)purerng.$(OBJEXT) src$(DIR_SEP)rev.$(OBJEXT) src$(DIR_SEP)stopcodes.$(OBJEXT) src$(DIR_SEP)timer.$(OBJEXT) app$(DIR_SEP)nmlfuzz.f90
 
 test_assert_dimension_false_1$(BINEXT): src$(DIR_SEP)$(BUILD).$(OBJEXT) src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) test$(DIR_SEP)test_assert_dimension_false_1.f90
 	$(FC) $(OFLAG) $@ $(FFLAGS) src$(DIR_SEP)$(BUILD).$(OBJEXT) src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) test$(DIR_SEP)test_assert_dimension_false_1.f90

src/geninput_io.f90
@@ -27,6 +27,7 @@ type :: config_type
     logical              :: write_return
     character(len=CL)    :: executable
     integer, allocatable :: acceptable_exit_codes(:)
+    real(WP)             :: run_time_threshold ! nmlfuzz will keep inputs that take longer than this time to run in seconds
     
     ! Write code for multiple namelist groups of the same name, like in `read_input_variable_namelists` here
     ! TODO: `logical :: multiple_namelist_groups`
@@ -84,9 +85,11 @@ subroutine read_config_namelist(input_file, config, rc)
     logical           :: write_return
     character(len=CL) :: executable ! for nmlfuzz
     integer           :: acceptable_exit_codes(100) ! for nmlfuzz
+    real(WP)          :: run_time_threshold
     
     namelist /geninput_config/ output_file_prefix, namelist_group, type_name, config_variable, kind_parameter, &
-                                write_tex, write_md, uq, ga, write_return, executable, acceptable_exit_codes
+                                write_tex, write_md, uq, ga, write_return, &
+                                executable, acceptable_exit_codes, run_time_threshold ! for nmlfuzz
     
     output_file_prefix    = ""
     namelist_group        = ""
@@ -100,6 +103,7 @@ subroutine read_config_namelist(input_file, config, rc)
     write_return          = .true.
     executable            = ""
     acceptable_exit_codes = MAX_EXIT_CODE + 1
+    run_time_threshold    = huge(1.0_WP)
     
     open(newunit=nml_unit, file=trim(input_file), status="old", action="read", delim="quote")
     read(unit=nml_unit, nml=geninput_config, iostat=rc_nml, iomsg=nml_error_message)
@@ -112,10 +116,11 @@ subroutine read_config_namelist(input_file, config, rc)
     end if
     
     rc = 0
-    call check(len(trim(output_file_prefix)) > 0, "output_file_prefix must be defined", rc)
-    call check(len(trim(namelist_group))     > 0, "namelist_group must be defined", rc)
-    call check(len(trim(executable))         > 0, "executable must be defined", rc)
-    call check(n_acceptable_exit_codes       > 0, "acceptable_exit_codes must be defined", rc)
+    call check(len(trim(output_file_prefix)) > 0,      "output_file_prefix must be defined", rc)
+    call check(len(trim(namelist_group))     > 0,      "namelist_group must be defined", rc)
+    call check(len(trim(executable))         > 0,      "executable must be defined", rc)
+    call check(n_acceptable_exit_codes       > 0,      "acceptable_exit_codes must be defined", rc)
+    call check(run_time_threshold            > 0.0_WP, "run_time_threshold can not be zero or negative", rc)
     
     use_type = len(trim(type_name)) > 0
     
@@ -131,6 +136,7 @@ subroutine read_config_namelist(input_file, config, rc)
     config%ga                 = ga
     config%write_return       = write_return
     config%executable         = executable
+    config%run_time_threshold = run_time_threshold
     
     if (platform() == PLATFORM_WINDOWS) then
         call convert_path_unix_to_win(config%output_file_prefix)

commit ca062709ba5373346335e0d287bcc04678490b92

fix: Made nmlfuzz not mark every case as a failure after a certain time period.
fixes: e38a44033ef53989e2299d09bb959a2925d69dcb ("feat: Made nmlfuzz check for long run times.")

app/nmlfuzz.f90
@@ -111,6 +111,7 @@ fuzzer_loop: do
     call wtime%stop()
     
     result_is_acceptable = wtime%read() < config%run_time_threshold
+    call wtime%reset()
     
     if (result_is_acceptable) then
         ! Check exit code to see if it's acceptable.