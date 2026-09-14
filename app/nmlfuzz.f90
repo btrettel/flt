btrettel/flt

commit 2e0392af01c0ec90a8e9e60152cb82e48a1a5315

fix: Made ga.f90 work properly if all are unfeasible and f_max=0.
feat: Made nmlfuzz's unguided mode work in parallel with OpenMP.

feat: Made nmlfuzz's output quiet by default.

app/nmlfuzz.f90
@@ -72,13 +72,12 @@ subroutine get_nml_filename(nml_file)
 end subroutine get_nml_filename
 
 subroutine unguided_fuzzer()
+    !$ use omp_lib
     use prec, only: WP
-    use purerng, only: rng_type
     use checks, only: is_close
     use timer, only: timer_type
     use ga, only: STOP_NOW_FILE
     
-    type(rng_type)    :: rng
     character(len=CL) :: nml_file
     integer           :: out_unit, i_var, x_integer, exit_code, i_exit_code
     character(4)      :: type4
@@ -86,8 +85,8 @@ subroutine unguided_fuzzer()
     logical           :: run_time_exceeded, bad_exit_code, stop_now_detected, out_file_exists
     type(timer_type)  :: wtime
     
-    call rng%random_seed()
-
+    !$omp parallel
+    !$omp do
     fuzzer_loop: do
         call get_nml_filename(nml_file)
         
@@ -96,7 +95,7 @@ subroutine unguided_fuzzer()
         write(unit=out_unit, fmt="(2a)") "&", trim(config%namelist_group)
         var_loop: do i_var = 1, size(input_variables)
             if (input_variables(i_var)%fuzz) then
-                call rng%random_number(x)
+                call random_number(x)
                 
                 type4 = input_variables(i_var)%type_definition(1:4)
                 select case (type4)
@@ -138,7 +137,7 @@ subroutine unguided_fuzzer()
         close(unit=out_unit)
         
         call wtime%start()
-        call execute_command_line(config%executable // " " // nml_file, exitstat=exit_code)
+        call execute_command_line(config%executable // " " // nml_file // " > /dev/null 2>&1", exitstat=exit_code)
         call wtime%stop()
         
         inquire(file=trim(nml_file)//".out", exist=out_file_exists)
@@ -175,6 +174,8 @@ subroutine unguided_fuzzer()
             exit
         end if
     end do fuzzer_loop
+    !$omp end do
+    !$omp end parallel
 end subroutine unguided_fuzzer
 
 subroutine guided_fuzzer()
@@ -205,10 +206,13 @@ subroutine guided_fuzzer()
     end do
     
     call standard_ga_config(n_genes, ga_config)
+    !ga_config%n_genes = n_genes
+    !ga_config%n_pop   = 2
     allocate(ga_config%lb(ga_config%n_genes))
     allocate(ga_config%ub(ga_config%n_genes))
-    ga_config%n_gener  = 100000
-    ga_config%progress = .false.
+    !ga_config%n_gener  = 2
+    ga_config%progress = .true.
+    ga_config%log_all  = .true.
     ga_config%stop_if_all_unfeasible = .false.
     
     var_loop: do i_gene = 1, n_genes
@@ -288,7 +292,7 @@ subroutine guided_fuzzer_objfun(chromo, f, sum_g)
     ! Run the program.
     
     call wtime%start()
-    call execute_command_line(config%executable // " " // nml_file, exitstat=exit_code)
+    call execute_command_line(config%executable // " " // nml_file // " > /dev/null 2>&1", exitstat=exit_code)
     call wtime%stop()
     
     ! Evaluate the output.

src/ga.f90
@@ -248,7 +248,7 @@ subroutine evaluate(config, objfun, pop)
     type(pop_type), intent(in out)   :: pop
     
     integer  :: i_pop
-    real(WP) :: f_max
+    real(WP) :: f_max, f_scale
     logical  :: best_pop_indiv_set
     
     interface
@@ -304,6 +304,7 @@ subroutine evaluate(config, objfun, pop)
     
     ! deb_efficient_2000 p. 317: > If no feasible solution exists in a population, $f_max$ is set to zero.
     ! I don't like this as `f` could normally be above 0.
+    ! Then the `best_ever_indiv` could be wrong.
     ! I decided to stop with an error by default in this situation.
     if (.not. best_pop_indiv_set) then
         if (config%stop_if_all_unfeasible) then
@@ -314,6 +315,12 @@ subroutine evaluate(config, objfun, pop)
         end if
     end if
     
+    if (is_close(f_max, 0.0_WP)) then
+        f_scale = 1.0_WP
+    else
+        f_scale = abs(f_max)
+    end if
+    
     ! set `f` for indivs that had constraint violations
     ! See deb_efficient_2000 eq. 4.
     do concurrent (i_pop = 1:config%n_pop)
@@ -321,12 +328,18 @@ subroutine evaluate(config, objfun, pop)
             ! Infeasible individuals have their fitness recalculated based on the current population.
             ! This is regardless of whether they were `set` before `evaluate` was called.
             ! This avoid issues from the fitness depending on the population.
-            pop%indivs(i_pop)%f   = f_max + pop%indivs(i_pop)%sum_g*abs(f_max)
+            pop%indivs(i_pop)%f   = f_max + pop%indivs(i_pop)%sum_g*f_scale
             pop%indivs(i_pop)%set = .true.
+            
+            if (pop%best_pop_indiv%f > pop%indivs(i_pop)%f) then
+                pop%best_pop_indiv = pop%indivs(i_pop)
+                best_pop_indiv_set = .true.
+            end if
         end if
     end do
     
     ! set best ever individual
+    call assert(best_pop_indiv_set, "ga (evaluate): best individual not set")
     if (pop%best_ever_indiv%f > pop%best_pop_indiv%f) then
         pop%best_ever_indiv = pop%best_pop_indiv
     end if

test/test_ga.f90
@@ -373,6 +373,7 @@ subroutine test_optimize_ga(tests)
     integer :: rc
     
     config%n_genes = 2
+    config%n_gener = 1000
     allocate(config%lb(config%n_genes))
     allocate(config%ub(config%n_genes))
     config%lb(1) = -20.0_WP
@@ -386,8 +387,7 @@ subroutine test_optimize_ga(tests)
     
     call tests%integer_eq(rc, 0, "optimize, rc")
     
-    ! characterization test
-    call tests%real_eq(pop%best_ever_indiv%f, 9.858531831321804_WP, "optimize, pop%best_ever_indiv%f", abs_tol=1.0e-12_WP)
+    call tests%real_lt(pop%best_ever_indiv%f, 1.0_WP, "optimize, pop%best_ever_indiv%f")
 end subroutine test_optimize_ga
 
 subroutine test_comparison_constraints(tests)

commit 7e6dba98dbb0738702a4348c98bed213cce4a852

fix: Made unguided fuzzer serial again due to OpenMP requirements not met. Removed code setting a variable that did not exist.
Fixes: 2e0392af01c0ec90a8e9e60152cb82e48a1a5315 ("fix: Made ga.f90 work properly if all are unfeasible and f_max=0.")

app/nmlfuzz.f90
@@ -72,12 +72,13 @@ subroutine get_nml_filename(nml_file)
 end subroutine get_nml_filename
 
 subroutine unguided_fuzzer()
-    !$ use omp_lib
     use prec, only: WP
+    use purerng, only: rng_type
     use checks, only: is_close
     use timer, only: timer_type
     use ga, only: STOP_NOW_FILE
     
+    type(rng_type)    :: rng
     character(len=CL) :: nml_file
     integer           :: out_unit, i_var, x_integer, exit_code, i_exit_code
     character(4)      :: type4
@@ -85,8 +86,8 @@ subroutine unguided_fuzzer()
     logical           :: run_time_exceeded, bad_exit_code, stop_now_detected, out_file_exists
     type(timer_type)  :: wtime
     
-    !$omp parallel
-    !$omp do
+    call rng%random_seed()
+    
     fuzzer_loop: do
         call get_nml_filename(nml_file)
         
@@ -95,7 +96,7 @@ subroutine unguided_fuzzer()
         write(unit=out_unit, fmt="(2a)") "&", trim(config%namelist_group)
         var_loop: do i_var = 1, size(input_variables)
             if (input_variables(i_var)%fuzz) then
-                call random_number(x)
+                call rng%random_number(x)
                 
                 type4 = input_variables(i_var)%type_definition(1:4)
                 select case (type4)
@@ -174,8 +175,6 @@ subroutine unguided_fuzzer()
             exit
         end if
     end do fuzzer_loop
-    !$omp end do
-    !$omp end parallel
 end subroutine unguided_fuzzer
 
 subroutine guided_fuzzer()
@@ -212,7 +211,6 @@ subroutine guided_fuzzer()
     allocate(ga_config%ub(ga_config%n_genes))
     !ga_config%n_gener  = 2
     ga_config%progress = .true.
-    ga_config%log_all  = .true.
     ga_config%stop_if_all_unfeasible = .false.
     
     var_loop: do i_gene = 1, n_genes