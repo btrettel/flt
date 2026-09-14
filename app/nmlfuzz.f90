btrettel/flt

commit efebb2ede7c573ddb326b84ee98874c4081e0579

feat: Got a crude guided fuzzer running.

app/nmlfuzz.f90
@@ -15,12 +15,14 @@ use stopcodes, only: EX_OK
 implicit none
 
 integer, parameter :: NMLFUZZ_UNGUIDED_MODE = 1
-!integer, parameter :: NMLFUZZ_GUIDED_MODE   = 2
+integer, parameter :: NMLFUZZ_GUIDED_MODE   = 2
 
 character(len=CL) :: input_file
 type(config_type) :: config
 integer           :: rc_config, rc_input_variables
+
 type(input_variable_type), allocatable :: input_variables(:)
+integer, allocatable :: input_variable_indexes(:)
 
 ! Read all namelists and exit if any have issues.
 call get_input_file_name_from_cli("nmlfuzz", input_file)
@@ -41,8 +43,8 @@ call sort_input_variables(input_variables)
 select case (config%nmlfuzz_mode)
     case (NMLFUZZ_UNGUIDED_MODE)
         call unguided_fuzzer()
-!    case (NMLFUZZ_GUIDED_MODE)
-!        call guided_fuzzer()
+    case (NMLFUZZ_GUIDED_MODE)
+        call guided_fuzzer()
     case default
         error stop "Invalid nmlfuzz_mode."
 end select
@@ -51,7 +53,7 @@ stop EX_OK, quiet=.true.
 
 contains
 
-subroutine get_random_filename(nml_file)
+subroutine get_nml_filename(nml_file)
     use purerng, only: rng_type
     
     character(len=CL), intent(out) :: nml_file
@@ -61,12 +63,12 @@ subroutine get_random_filename(nml_file)
     
     do i = 1, 999
         write(unit=nml_file, fmt="(2a, i3.3, a)") trim(config%namelist_group), "_", i, ".nml"
-        inquire(file=nml_file, exist=file_exists)
+        inquire(file=trim(nml_file), exist=file_exists)
         if (.not. file_exists) exit
     end do
     
     if (file_exists) error stop "nmlfuzz (get_nml_filename): could not create file"
-end subroutine get_random_filename
+end subroutine get_nml_filename
 
 subroutine unguided_fuzzer()
     use prec, only: WP
@@ -80,13 +82,13 @@ subroutine unguided_fuzzer()
     integer           :: out_unit, i_var, x_integer, exit_code, i_exit_code
     character(4)      :: type4
     real(WP)          :: x, x_real
-    logical           :: result_is_acceptable, stop_now_detected
+    logical           :: run_time_exceeded, bad_exit_code, stop_now_detected
     type(timer_type)  :: wtime
     
     call rng%random_seed()
 
     fuzzer_loop: do
-        call get_random_filename(nml_file)
+        call get_nml_filename(nml_file)
         
         open(newunit=out_unit, action="write", status="replace", position="rewind", &
                 file=trim(nml_file))
@@ -138,24 +140,23 @@ subroutine unguided_fuzzer()
         call execute_command_line(config%executable // " " // nml_file, exitstat=exit_code)
         call wtime%stop()
         
-        result_is_acceptable = wtime%read() < config%run_time_threshold
+        run_time_exceeded = wtime%read() > config%run_time_threshold
         call wtime%reset()
         
-        if (result_is_acceptable) then
-            ! Check exit code to see if it's acceptable.
-            exit_code_loop: do i_exit_code = 1, size(config%acceptable_exit_codes)
-                if (exit_code == config%acceptable_exit_codes(i_exit_code)) then
-                    result_is_acceptable = .true.
-                    exit exit_code_loop
-                end if
-            end do exit_code_loop
-        end if
+        bad_exit_code = .true.
+        ! Check exit code to see if it's acceptable.
+        exit_code_loop: do i_exit_code = 1, size(config%acceptable_exit_codes)
+            if (exit_code == config%acceptable_exit_codes(i_exit_code)) then
+                bad_exit_code = .false.
+                exit exit_code_loop
+            end if
+        end do exit_code_loop
         
-        if (result_is_acceptable) then
+        if (run_time_exceeded .or. bad_exit_code) then
+            print "(2a)", "Failure detected, file kept: ", trim(nml_file)
+        else
             open(newunit=out_unit, status="old", file=trim(nml_file))
             close(unit=out_unit, status="delete")
-        else
-            print "(2a)", "Failure detected, file kept: ", trim(nml_file)
         end if
         
         ! detect `stop_now` file and quit if found
@@ -169,27 +170,162 @@ subroutine unguided_fuzzer()
     end do fuzzer_loop
 end subroutine unguided_fuzzer
 
-!subroutine unguided_fuzzer()
-!    use prec, only: CL
+subroutine guided_fuzzer()
+    use ga, only: ga_config_type, pop_type, standard_ga_config, init_pop, optimize_ga
+    use purerng, only: rng_type
     
-!    type(rng_type) :: rng
+    type(ga_config_type) :: ga_config
+    type(rng_type)       :: rng
+    type(pop_type)       :: pop
+    integer              :: i_var, n_genes, i_gene, rc
 
+    ! get n_genes
+    n_genes = 0
+    do i_var = 1, size(input_variables)
+        if (input_variables(i_var)%fuzz) n_genes = n_genes + 1
+    end do
     
-!end subroutine unguided_fuzzer
+    if (n_genes == 0) error stop "No variables to fuzz?"
+    
+    ! Make conversion table between `chromo` and `input_variables`.
+    allocate(input_variable_indexes(n_genes))
+    i_gene = 0
+    do i_var = 1, size(input_variables)
+        if (input_variables(i_var)%fuzz) then
+            i_gene = i_gene + 1
+            input_variable_indexes(i_gene) = i_var
+        end if
+    end do
+    
+    call standard_ga_config(n_genes, ga_config)
+    allocate(ga_config%lb(ga_config%n_genes))
+    allocate(ga_config%ub(ga_config%n_genes))
+    ga_config%n_gener  = 100000
+    ga_config%progress = .false.
+    ga_config%stop_if_all_unfeasible = .false.
+    
+    var_loop: do i_gene = 1, n_genes
+        i_var = input_variable_indexes(i_gene)
+        
+        ga_config%lb(i_gene) = input_variables(i_var)%fuzz_range(1)
+        ga_config%ub(i_gene) = input_variables(i_var)%fuzz_range(2)
+    end do var_loop
+    
+    call rng%random_seed()
+    call init_pop(ga_config, rng, pop)
+    call optimize_ga(ga_config, rng, guided_fuzzer_objfun, pop, rc)
+end subroutine guided_fuzzer
 
-!pure subroutine unguided_fuzzer_objfun(chromo, f, sum_g)
-!    use timer, only: timer_type
+subroutine guided_fuzzer_objfun(chromo, f, sum_g)
+    use prec, only: WP
+    use timer, only: timer_type
+    use checks, only: assert
     
-!    real(WP), intent(in)  :: chromo(:)
-!    real(WP), intent(out) :: f
-!    real(WP), intent(out) :: sum_g
+    real(WP), intent(in)  :: chromo(:)
+    real(WP), intent(out) :: f
+    real(WP), intent(out) :: sum_g
     
-!    call wtime%start()
-!    call execute_command_line(config%executable // " " // nml_file, exitstat=exit_code)
-!    call wtime%stop()
+    integer           :: n_genes, out_unit, i_gene, x_integer, exit_code, in_unit, i_var, i_exit_code
+    character(len=CL) :: nml_file
+    real(WP)          :: x_real
+    type(timer_type)  :: wtime
+    logical           :: run_time_exceeded, bad_exit_code, out_file_exists
+    character(4)      :: type4
     
-!    result_is_acceptable = wtime%read() < config%run_time_threshold
-!    call wtime%reset()
-!end subroutine unguided_fuzzer_objfun
+    n_genes = size(chromo)
+    
+    ! Write .nml file.
+    call get_nml_filename(nml_file)
+    
+    open(newunit=out_unit, action="write", status="replace", position="rewind", &
+            file=trim(nml_file))
+    write(unit=out_unit, fmt="(2a)") "&", trim(config%namelist_group)
+    var_loop: do i_gene = 1, n_genes
+        i_var = input_variable_indexes(i_gene)
+        call assert(input_variables(i_var)%fuzz, &
+                        "nmlfuzz (unguided_fuzzer_objfun): Variable not marked as fuzzable?", print_integer=[i_gene, i_var])
+        
+        type4 = input_variables(i_var)%type_definition(1:4)
+        select case (type4)
+            case ("inte")
+                x_integer = nint(chromo(i_gene))
+                
+                write(unit=out_unit, fmt="(2a, i0, 2a)") trim(input_variables(i_var)%variable_name), " = ", x_real, &
+                                                            " ! ", trim(input_variables(i_var)%txt_unit)
+            case ("real", "type")
+                x_real = chromo(i_gene)
+                
+                write(unit=out_unit, fmt="(2a, g0, 2a)") trim(input_variables(i_var)%variable_name), " = ", x_real, &
+                                                            " ! ", trim(input_variables(i_var)%txt_unit)
+            case ("char")
+                error stop "nmlfuzz can't handle char variables at present."
+            case ("logi")
+                x_integer = nint(chromo(i_gene))
+                
+                if (x_integer == 0) then
+                    write(unit=out_unit, fmt="(4a)") trim(input_variables(i_var)%variable_name), " = .false.", &
+                                                            " ! ", trim(input_variables(i_var)%txt_unit)
+                else if (x_integer == 1) then
+                    write(unit=out_unit, fmt="(4a)") trim(input_variables(i_var)%variable_name), " = .true.", &
+                                                            " ! ", trim(input_variables(i_var)%txt_unit)
+                else
+                    error stop "Any element of fuzz_range for logicals must be 0.0 for false or 1.0 for true."
+                end if
+            case default
+                error stop "Invalid type definition."
+        end select
+    end do var_loop
+    write(unit=out_unit, fmt="(a)") "/"
+    close(unit=out_unit)
+    
+    ! Run the program.
+    
+    call wtime%start()
+    call execute_command_line(config%executable // " " // nml_file, exitstat=exit_code)
+    call wtime%stop()
+    
+    ! Evaluate the output.
+    
+    run_time_exceeded = wtime%read() > config%run_time_threshold
+    !print *, 1, run_time_exceeded, wtime%read(), config%run_time_threshold
+    
+    bad_exit_code = .true.
+    ! Check exit code to see if it's acceptable.
+    exit_code_loop: do i_exit_code = 1, size(config%acceptable_exit_codes)
+        if (exit_code == config%acceptable_exit_codes(i_exit_code)) then
+            bad_exit_code = .false.
+            exit exit_code_loop
+        end if
+    end do exit_code_loop
+    !print *, 2, bad_exit_code, exit_code
+    
+    inquire(file=trim(nml_file)//".out", exist=out_file_exists)
+    if (out_file_exists) then
+        open(newunit=in_unit, file=trim(nml_file)//".out", status="old", action="read")
+        read(unit=in_unit, fmt="(es24.17, 1x, es24.17)") f, sum_g
+        close(unit=in_unit)
+        open(newunit=out_unit, status="old", file=trim(nml_file)//".out")
+        close(unit=out_unit, status="delete")
+        !print *, 3, f, sum_g
+    else
+        ! TODO: For input validation errors, output a .out file listing the amount of the violation.
+        
+        f     = 0.0_WP
+        sum_g = 10.0_WP
+    end if
+    
+    ! One component of the objective function is run time. Longer run times are more likely bad.
+    f = f - wtime%read()
+    
+    if (run_time_exceeded .or. bad_exit_code) then
+        print "(2a)", "Failure detected, file kept: ", trim(nml_file)
+    else
+        open(newunit=out_unit, status="old", file=trim(nml_file))
+        close(unit=out_unit, status="delete")
+    end if
+    
+    !print *, 4, f, sum_g
+    call wtime%reset()
+end subroutine guided_fuzzer_objfun
 
 end program nmlfuzz

src/ga.f90
@@ -27,7 +27,7 @@ character(len=*), public, parameter :: STOP_NOW_FILE = "stop_now"
 integer, parameter          :: MAX_SAMPLES = 10000
 character(len=*), parameter :: GENER_FMT = "(i8)"
 
-type, public :: ga_config
+type, public :: ga_config_type
     integer :: n_genes = 0 ! number of genes (default set to zero to catch when not set)
     
     ! martins_engineering_2021 p. 309:
@@ -61,7 +61,7 @@ type, public :: ga_config
     
     logical          :: progress = .true., check_sum_g = .true.
     character(len=8) :: f_fmt = "f12.2"
-end type ga_config
+end type ga_config_type
 
 type, public :: indiv_type
     real(WP), allocatable :: chromo(:)
@@ -88,9 +88,9 @@ subroutine init_pop(config, rng, pop)
     use purerng, only: rng_type
     use checks, only: assert
     
-    type(ga_config), intent(in)    :: config
-    type(rng_type), intent(in out) :: rng
-    type(pop_type), intent(out)    :: pop
+    type(ga_config_type), intent(in) :: config
+    type(rng_type), intent(in out)   :: rng
+    type(pop_type), intent(out)      :: pop
     
     integer :: i_pop, i_gene
     
@@ -130,7 +130,7 @@ pure subroutine mutate_indiv(config, rng, indiv)
     use purerng, only: rng_type
     use checks, only: assert
     
-    type(ga_config), intent(in)      :: config
+    type(ga_config_type), intent(in) :: config
     type(rng_type), intent(in out)   :: rng
     type(indiv_type), intent(in out) :: indiv
     
@@ -179,7 +179,7 @@ pure subroutine cross_two_indivs(config, rng, indiv_1, indiv_2)
     use purerng, only: rng_type
     use checks, only: assert, assert_dimension
     
-    type(ga_config), intent(in)      :: config
+    type(ga_config_type), intent(in) :: config
     type(rng_type), intent(in out)   :: rng
     type(indiv_type), intent(in out) :: indiv_1, indiv_2
     
@@ -216,10 +216,10 @@ pure subroutine select_indiv(config, rng, pop, indiv)
     use purerng, only: rng_type
     use checks, only: assert, assert_dimension
     
-    type(ga_config), intent(in)    :: config
-    type(rng_type), intent(in out) :: rng
-    type(pop_type), intent(in out) :: pop
-    type(indiv_type), intent(out)  :: indiv
+    type(ga_config_type), intent(in) :: config
+    type(rng_type), intent(in out)   :: rng
+    type(pop_type), intent(in out)   :: pop
+    type(indiv_type), intent(out)    :: indiv
     
     integer :: nu, i_pop
     
@@ -244,8 +244,8 @@ subroutine evaluate(config, objfun, pop)
     use prec, only: WP
     use checks, only: assert, is_close
     
-    type(ga_config), intent(in)    :: config
-    type(pop_type), intent(in out) :: pop
+    type(ga_config_type), intent(in) :: config
+    type(pop_type), intent(in out)   :: pop
     
     integer  :: i_pop
     real(WP) :: f_max
@@ -338,10 +338,10 @@ subroutine optimize_ga(config, rng, objfun, pop, rc)
     use purerng, only: rng_type
     use checks, only: assert
     
-    type(ga_config), intent(in)    :: config
-    type(rng_type), intent(in out) :: rng
-    type(pop_type), intent(in out) :: pop
-    integer, intent(out)           :: rc ! TODO: return codes
+    type(ga_config_type), intent(in) :: config
+    type(rng_type), intent(in out)   :: rng
+    type(pop_type), intent(in out)   :: pop
+    integer, intent(out)             :: rc ! TODO: return codes
     
     integer :: i_gener, i_pop, out_unit
     logical :: stop_now_detected
@@ -447,8 +447,8 @@ pure subroutine constraint_gt(x, y, delta_scale, sum_g)
 end subroutine constraint_gt
 
 pure subroutine standard_ga_config(n_genes, config)
-    integer, intent(in)          :: n_genes
-    type(ga_config), intent(out) :: config
+    integer, intent(in)               :: n_genes
+    type(ga_config_type), intent(out) :: config
     
     config%n_genes = n_genes
     

test/test_ga.f90
@@ -29,14 +29,14 @@ contains
 
 subroutine test_init_pop(tests)
     use prec, only: WP
-    use ga, only: ga_config, pop_type, init_pop
+    use ga, only: ga_config_type, pop_type, init_pop
     use purerng, only: rng_type
     
     type(test_results_type), intent(in out) :: tests
     
-    type(ga_config) :: config
-    type(rng_type)  :: rng
-    type(pop_type)  :: pop
+    type(ga_config_type) :: config
+    type(rng_type)       :: rng
+    type(pop_type)       :: pop
     
     config%n_pop   = 2
     config%n_genes = 3
@@ -70,14 +70,14 @@ end subroutine test_init_pop
 
 subroutine test_mutate_indiv(tests)
     use prec, only: WP
-    use ga, only: ga_config, indiv_type, mutate_indiv
+    use ga, only: ga_config_type, indiv_type, mutate_indiv
     use purerng, only: rng_type
     
     type(test_results_type), intent(in out) :: tests
     
-    type(ga_config)  :: config
-    type(rng_type)   :: rng
-    type(indiv_type) :: indiv
+    type(ga_config_type) :: config
+    type(rng_type)       :: rng
+    type(indiv_type)     :: indiv
     
     real(WP) :: b, nu
     
@@ -123,14 +123,14 @@ end subroutine test_mutate_indiv
 
 subroutine test_cross_two_indivs(tests)
     use prec, only: WP
-    use ga, only: ga_config, indiv_type, cross_two_indivs
+    use ga, only: ga_config_type, indiv_type, cross_two_indivs
     use purerng, only: rng_type
     
     type(test_results_type), intent(in out) :: tests
     
-    type(ga_config)  :: config
-    type(rng_type)   :: rng
-    type(indiv_type) :: indiv_1, indiv_2
+    type(ga_config_type) :: config
+    type(rng_type)       :: rng
+    type(indiv_type)     :: indiv_1, indiv_2
     
     config%n_pop   = 1
     config%n_genes = 2
@@ -179,15 +179,15 @@ end subroutine test_cross_two_indivs
 
 subroutine test_select_indiv(tests)
     use prec, only: WP
-    use ga, only: ga_config, pop_type, indiv_type, select_indiv
+    use ga, only: ga_config_type, pop_type, indiv_type, select_indiv
     use purerng, only: rng_type
     
     type(test_results_type), intent(in out) :: tests
     
-    type(ga_config)  :: config
-    type(rng_type)   :: rng
-    type(pop_type)   :: pop
-    type(indiv_type) :: indiv
+    type(ga_config_type) :: config
+    type(rng_type)       :: rng
+    type(pop_type)       :: pop
+    type(indiv_type)     :: indiv
     
     config%n_pop = 2
     allocate(pop%indivs(config%n_pop))
@@ -242,12 +242,12 @@ end subroutine rosenbrock
 
 subroutine test_evaluate_1(tests)
     use prec, only: WP
-    use ga, only: ga_config, pop_type, evaluate
+    use ga, only: ga_config_type, pop_type, evaluate
     
     type(test_results_type), intent(in out) :: tests
     
-    type(ga_config) :: config
-    type(pop_type)  :: pop
+    type(ga_config_type) :: config
+    type(pop_type)       :: pop
     
     config%n_pop   = 2
     config%n_genes = 2
@@ -278,12 +278,12 @@ subroutine test_evaluate_2(tests)
     ! Check that `f` and `g_sum` are only set if `set = .false.`.
     
     use prec, only: WP
-    use ga, only: ga_config, pop_type, evaluate
+    use ga, only: ga_config_type, pop_type, evaluate
     
     type(test_results_type), intent(in out) :: tests
     
-    type(ga_config) :: config
-    type(pop_type)  :: pop
+    type(ga_config_type) :: config
+    type(pop_type)       :: pop
     
     config%n_pop   = 4
     config%n_genes = 2
@@ -361,14 +361,14 @@ end subroutine evaluate_2
 
 subroutine test_optimize_ga(tests)
     use prec, only: WP, I10
-    use ga, only: ga_config, pop_type, init_pop, optimize_ga
+    use ga, only: ga_config_type, pop_type, init_pop, optimize_ga
     use purerng, only: rng_type
     
     type(test_results_type), intent(in out) :: tests
     
-    type(ga_config) :: config
-    type(rng_type)  :: rng
-    type(pop_type)  :: pop
+    type(ga_config_type) :: config
+    type(rng_type)       :: rng
+    type(pop_type)       :: pop
     
     integer :: rc
     

commit 4873806f9b94605001df2f68a0f973879bb1a582

fix: Made nmlfuzz's guided fuzzer run for longer and reduced the size of a constraint violation to be O(1).
Fixes: efebb2ede7c573ddb326b84ee98874c4081e0579 ("feat: Got a crude guided fuzzer running."), 2e0392af01c0ec90a8e9e60152cb82e48a1a5315 ("2e0392af01c0ec90a8e9e60152cb82e48a1a5315")

app/nmlfuzz.f90
@@ -209,7 +209,7 @@ subroutine guided_fuzzer()
     !ga_config%n_pop   = 2
     allocate(ga_config%lb(ga_config%n_genes))
     allocate(ga_config%ub(ga_config%n_genes))
-    !ga_config%n_gener  = 2
+    ga_config%n_gener  = 100000
     ga_config%progress = .true.
     ga_config%stop_if_all_unfeasible = .false.
     
@@ -320,7 +320,7 @@ subroutine guided_fuzzer_objfun(chromo, f, sum_g)
         ! TODO: For input validation errors, output a .out file listing the amount of the violation.
         
         f     = 0.0_WP
-        sum_g = 10.0_WP
+        sum_g = 1.5_WP
     end if
     
     ! One component of the objective function is run time. Longer run times are more likely bad.

src/ga.f90
@@ -394,7 +394,7 @@ subroutine optimize_ga(config, rng, objfun, pop, rc)
     end if
     
     rc = 0
-    do i_gener = 1, config%n_gener
+    do i_gener = 1, config%n_gener ! SERIAL
         do i_pop = 1, config%n_pop, 2 ! SERIAL
             ! I don't have a parallel RNG right now, so this can't be parallelized at the moment.
             ! Not that parallelizing this would matter much anyway as it's quick.